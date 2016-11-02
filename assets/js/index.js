29e89ede1e79771978d6f826d1991a7640bd222a
Sam Walston <samjwalston@gmail.com>
2016-11-02 09:26:06 -0700

    Fixed shit

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// function ready() {
+
+// 	if (iOSversion()) {
+// 		var iframe = document.querySelector('#video');
+
+// 		iframe.width = '560';
+// 		iframe.height = '315';
+// 		iframe.src = 'https://www.youtube.com/embed/ooHhvZ2CUEg';
+// 	}
+// }
+
+// function iOSversion() {
+// 	return (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
+// }
+
+// document.addEventListener('DOMContentLoaded', ready);
