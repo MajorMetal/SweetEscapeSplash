58356be6b917e5148b5396d5d3529231bc879a38
Major Metal <kinney.matthewl@gmail.com>
2016-11-14 17:54:23 -0800

    testing site

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,21 @@
-function ready() {
-	document.removeEventListener('DOMContentLoaded', ready);
+// function ready() {
+// 	document.removeEventListener('DOMContentLoaded', ready);
 
 
-	postNotification();
+// 	postNotification();
 
-	if (iOSversion()) {
-		var iframe = document.getElementsByClassName('video');
+// 	if (iOSversion()) {
+// 		var iframe = document.getElementsByClassName('video');
 
-		iframe.width = '560';
-		iframe.height = '315';
-		iframe.src = 'https://www.youtube.com/embed/ooHhvZ2CUEg';
-	}
-}
+// 		iframe.width = '560';
+// 		iframe.height = '315';
+// 		iframe.src = 'https://www.youtube.com/embed/ooHhvZ2CUEg';
+// 	}
+// }
 
-function iOSversion() {
-	return (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
-}
+// function iOSversion() {
+// 	return (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
+// }
 
 $(document).ready(function(){
  
@@ -46,22 +46,22 @@ $(document).ready(function(){
 // }
 
 
-$(document).ready(function(){
-  $(".button").click(function(e){
-      e.preventDefault();
-    $.ajax({type: "POST",
-            url: "https://hooks.slack.com/services/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",
-            data: "payload=" + JSON.stringify({ 
-            	  "channel": "#sweetescape", 
-            	  "username": "webhookbot", 
-            	  "text": "Some one is in sweetescape.", 
-            	  "icon_emoji": ":ghost:" }),
-            dataType: 'json',
-            success:function(result){
-      		console.log("It Worked");
-    }});
-  });
-});
+// $(document).ready(function(){
+//   $(".button").click(function(e){
+//       e.preventDefault();
+//     $.ajax({type: "POST",
+//             url: "https://hooks.slack.com/services/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",
+//             data: "payload=" + JSON.stringify({ 
+//             	  "channel": "#sweetescape", 
+//             	  "username": "webhookbot", 
+//             	  "text": "Some one is in sweetescape.", 
+//             	  "icon_emoji": ":ghost:" }),
+//             dataType: 'json',
+//             success:function(result){
+//       		console.log("It Worked");
+//     }});
+//   });
+// });
  //document.body.addEventListener('DOMContentLoaded', ready);
 
 
