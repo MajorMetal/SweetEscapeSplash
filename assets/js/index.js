93a57b61d9b8b4634e28a7234753670c9a456856
Major Metal <kinney.matthewl@gmail.com>
2016-11-21 23:19:41 -0800

    new logo

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,20 +18,7 @@
 // }
 
 
-// $(document).ready(function(){
- 
-//     $.ajax({type: "POST",
-//             url: "https://hooks.slack.com/services/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",
-//             data: "payload=" + JSON.stringify({ 
-//             	  "channel": "#sweetescape", 
-//             	  "username": "webhookbot", 
-//             	  "text": "Some one is in sweetescape.", 
-//             	  "icon_emoji": ":ghost:" }),
-//             dataType: 'json',
-//             success:function(result){
-//       		console.log("It Worked");
-//     }});
-// });
+
 
 
 
